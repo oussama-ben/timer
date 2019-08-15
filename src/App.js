433931da6e40bd961f5a02a9372b50b81e74cf29
oussama-ben <oussama.bensalah@supinfo.com>
2019-08-15 15:24:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from './timer';
+import './timer.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Timer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/timer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './timer.css';
+
+
+
+function Timer() {
+
+    return (
+      <div className="Container">
+      
+        
+        <h1 className="titre">00 :30 :00</h1>
+        <p className="temps">Hour  Minute  Second</p>
+        
+        
+      
+    </div>
+
+    );
+}
+export default Timer;
